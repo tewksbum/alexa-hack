948a6684b6cba8f7bfd2785691af38b1232bf422
ScottLWasserman-Stuzo <scott.wasserman@stuzo.com>
2016-10-22 21:23:52 -0400

    Update index.js

--- AlexaLambdaLayer/src/index.js
+++ AlexaLambdaLayer/src/index.js
@@ -1,155 +1,166 @@
-// Brown Bear Alexa sample skill
-// see https://amzn.com/0805047905
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-// var AWS = require('aws-sdk');
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
 
-exports.handler = function( event, context ) {
-    var say = "";
-    var endsession = false;
-    var sessionAttributes = {};
-    var mySystoric = "";
-    var myDestolic = "";
+        http://aws.amazon.com/apache2.0/
 
-    if (event.session.attributes) {
-        sessionAttributes = event.session.attributes;
-    }
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
 
-    if (event.request.type === "LaunchRequest") {
-        say = "Hello, what is your Systoric and Destolic blood pressure";
+/**
+ * This simple sample has no external dependencies or session management, and shows the most basic
+ * example of how to create a Lambda function for handling Alexa Skill requests.
+ *
+ * Examples:
+ * One-shot model:
+ *  User: ""
+ *  Alexa: ""
+ */
 
-    } else {
-        var IntentName = event.request.intent.name;
+/**
+ * App ID for the skill
+ */
+var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 
-        if (IntentName === "RecordBloodPressureReadingIntent") {
+/**
+ * The AlexaSkill prototype and helper functions
+ */
+var AlexaSkill = require('./AlexaSkill');
 
-            if(event.request.intent.slots.Color.value && event.request.intent.slots.Animal.value) {
-
-                mySystoric  = event.request.intent.slots.Systolic.value;
-                myDestolic = event.request.intent.slots.Diastolic.value;
-
-                if (!sessionAttributes.myList)  {sessionAttributes.myList = []; }
-
-                sessionAttributes.myList.push(mySystoric + " " + myDestolic);
-
-                say = mySystoric + " " + myDestolic + ", " + mySystoric + " " + myDestolic +  ", what do you see? ";
-
-            } else {
-                say = "you can say things like, I see a red bird looking at me";
-            }
-
-        } else if (IntentName === "EndIntent") {
-            say = "We see a " + sessionAttributes.myList.toString() + " looking at us.  Thank you for playing!";
-            endsession = true;
-
-        }
-    }
-
-    var response = {
-        outputSpeech: {
-            type: "SSML",
-            ssml: "<speak>" + say + "</speak>"
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "SSML",
-                ssml: "<speak>Please try again. " + say + "</speak>"
-            }
-        },
-        card: {
-            type: "Simple",
-            title: "My Card Title",
-            content: "My Card Content, displayed on the Alexa Companion mobile App or alexa.amazon.com"
-        },
-
-        shouldEndSession: endsession
-    };
-
-
-
-    Respond(  // Respond with normal speech only
-        function() {context.succeed( {sessionAttributes: sessionAttributes, response: response } ); }
-    );
-
-
-    // --------- Uncomment for AWS SQS Integration -------------------------------------------------
-    //RespondSendSqsMessage(  // use this to send a new message to an SQS Queue
-    //    {
-    //        MessageBody:  "https://www.google.com/search?tbm=isch&q=" + myColor + "%20" + myAnimal  // Message Body (Image Search URL)
-    //    },
-    //     function() {context.succeed( {sessionAttributes: sessionAttributes, response: response } ); }
-    //);
-
-
-    // --------- Uncomment for AWS IOT Integration -------------------------------------------------
-    //RespondUpdateIotShadow(  // use this to update an IoT device state
-    //    {
-    //        IOT_THING_NAME: "MyDevice",
-    //        IOT_DESIRED_STATE: {"pump":1}  // or send spoken slot value detected
-    //    },
-    //    function() {context.succeed( {sessionAttributes: sessionAttributes, response: response } ); }
-    //);
-
-
+/**
+ * SpaceGeek is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
+ */
+var Fact = function () {
+    AlexaSkill.call(this, APP_ID);
 };
 
-// -----------------------------------------------------------------------------
+// Extend AlexaSkill
+Fact.prototype = Object.create(AlexaSkill.prototype);
+Fact.prototype.constructor = Fact;
 
-function Respond(callback) {
-    callback();
-}
+Fact.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    //console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
+    // any initialization logic goes here
+};
 
-function RespondSendSqsMessage(sqs_params, callback) {
+Fact.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    //console.log("onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    handleHelpRequest(response);
+};
 
-    sqs_params.QueueUrl = "https://sqs.us-east-1.amazonaws.com/333304289684/AlexaQueue";
+/**
+ * Overridden to show that a subclass can override this function to teardown session state.
+ */
+Fact.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    //console.log("onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
+    // any cleanup logic goes here
+};
 
-    var sqs = new AWS.SQS({region : 'us-east-1'});
+Fact.prototype.intentHandlers = {
+    "RecordBloodPressureReadingIntent": function (intent, session, response) {
+        handleRecordBloodPressureReadingRequest(intent,response);
+    },
 
+    "AMAZON.HelpIntent": function (intent, session, response) {
+        handleHelpRequest(response);
+    },
 
-    sqs.sendMessage(sqs_params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else {
-            console.log("success calling sqs sendMessage");
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
 
-            callback();  // after performing SQS send, execute the caller's context.succeed function to complete
-        }
-    });
-
-}
-
-
-function RespondUpdateIotShadow(iot_config, callback) {
-
-    iot_config.IOT_BROKER_ENDPOINT      = "https://A2ESHRCP6U0Y0C.iot.us-east-1.amazonaws.com".toLowerCase();
-    iot_config.IOT_BROKER_REGION       = "us-east-1";
-
-
-    var iotData = new AWS.IotData({endpoint: iot_config.IOT_BROKER_ENDPOINT});
-
-    //Set the pump to 1 for activation on the device
-    var payloadObj={ "state":
-    { "desired":
-    iot_config.IOT_DESIRED_STATE // {"pump":1}
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
     }
-    };
+};
 
-    //Prepare the parameters of the update call
-    var paramsUpdate = {
-        "thingName" : iot_config.IOT_THING_NAME,
-        "payload" : JSON.stringify(payloadObj)
-    };
-    // see results in IoT console, MQTT client tab, subscribe to $aws/things/YourDevice/shadow/update/delta
-
-    //Update Device Shadow
-    iotData.updateThingShadow(paramsUpdate, function(err, data) {
-        if (err){
-            console.log(err.toString());
-        }
-        else {
-            console.log("success calling IoT updateThingShadow");
-            callback();  // after performing Iot action, execute the caller's context.succeed function to complete
-        }
-    });
-
-
-
+function handleHelpRequest(response) {
+    //response.ask("I need two blood pressure readings, systolic and diastolic, please tell me your blood pressure with both numbers.?", "What can I help you with?");
+    response.ask("I need two blood pressure readings, systolic and diastolic, please tell me your blood pressure with both numbers.");
 }
+
+/**
+ * Gets a random new fact from the list and returns to the user.
+ */
+function handleRecordBloodPressureReadingRequest(intent,response) {
+    // Get a random space fact from the space facts list
+
+
+    if ((intent.slots && intent.slots.Systolic && intent.slots.Systolic.value) && (intent.slots && intent.slots.Diastolic && intent.slots.Diastolic.value)) {
+       console.log("Found " + intent.slots.Systolic.value + " over " + intent.slots.Diastolic.value);
+       
+    var http = require('http');
+var querystring = require('querystring');
+    
+    var post_data = querystring.stringify({
+      'systolic' : intent.slots.Systolic.value,
+      'diastolic': intent.slots.Diastolic
+  });
+
+  // An object of options to indicate where to post to
+  var post_options = {
+      host: 'alexaphillyhack.herokuapp.com',
+      port: '80',
+      path: '/api/readings',
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+
+  // Set up the request
+  var post_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log('Response: ' + chunk);
+          var speechOutput;
+          
+          if (intent.slots.Systolic.value > 180) {
+              speechOutput = "if you're reading was " + intent.slots.Systolic.value + " over " + intent.slots.Diastolic.value + " you should go to the emergency room. Can I call you an ambulance?"
+          }
+          else {
+              if (intent.slots.Systolic.value < 120) {
+              //speechOutput = "got " + intent.slots.Systolic.value + " over " + intent.slots.Diastolic.value;
+              speechOutput = "you're reading of " + intent.slots.Systolic.value + " over " + intent.slots.Diastolic.value + " is Normal";
+              }
+              else {
+                speechOutput = "you're reading of " + intent.slots.Systolic.value + " over " + intent.slots.Diastolic.value + " is Prehypertensive";
+                
+              }
+          }
+          
+          
+        response.tell(speechOutput);
+      });
+  });
+
+  // post the data
+  post_req.write(post_data);
+  post_req.end();
+
+
+    
+        
+    }
+    else {
+        console.log("Couldn't find both numbers");
+        handleHelpRequest(response);
+    }
+
+   
+}
+
+// Create the handler that responds to the Alexa Request.
+exports.handler = function (event, context) {
+    // Create an instance of the SpaceGeek skill.
+    var fact = new Fact();
+    fact.execute(event, context);
+};
+
